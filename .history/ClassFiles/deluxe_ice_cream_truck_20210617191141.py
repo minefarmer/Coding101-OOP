d5655977559e3bd1acf6a4f185a6121cc3b05ce4
minefarmer <pgoldfarm@yahoo.com>
2021-06-17 19:28:06 -0500

    bye

---
+++ .history/ClassFiles/deluxe_ice_cream_truck_20210617191141.py
@@ -0,0 +1,49 @@
+
+
+
+from sympy import real_root
+
+
+class Icecream:
+
+    max_scoops = 3
+
+    def __init__(self):
+        self.scoops = 0
+
+    def eat(self, scoops):
+        if self.scoops < scoops:
+            print("Not enough bites left!")
+        else:
+            self.scoops -= scoops
+
+    def add(self, scoops):
+        self.scoops += scoops # new - in-place op
+        if self.scoops > self.max_scoops:
+            self.scoops = 0
+            print("Too many scoops! Dropped ice cream.")
+
+class IceCreamTruck:
+    def order(self, scoops):
+        self.sold = 0
+
+    
+
+    def order(self, scoops):
+        ice_cream = Icecream()
+        self.add(ice_cream, scoops)
+        return ice_cream
+
+    def add(self, ice_cream, scoops):
+        ice_cream.add(scoops)
+        self.sold += scoops
+
+
+
+
+class DeluxeIceCreamTruck(iceCreamTruck):
+    def order(self, scoops):
+        ice_cream = super().order(scoops)
+        ice_cream.add(1)
+        return ice_cream
+
